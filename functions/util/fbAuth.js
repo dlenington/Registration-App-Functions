f9714fbe7918b05bc6dc41e98e7d46e58c8188e7
Dan Lenington <danlening02@gmail.com>
2020-02-13 15:09:03 -0600

    Middleware
    Create middleware function to decode token from client and set response headers for axios.

--- functions/util/fbAuth.js
+++ functions/util/fbAuth.js
@@ -11,4 +11,25 @@ module.exports = (req, res, next) => {
     console.error("No token found");
     return res.status(403).json({ error: "Unauthorized" });
   }
+
+  admin
+    .auth()
+    .verifyIdToken(idToken)
+    .then(decodedToken => {
+      req.user = decodedToken;
+      return db
+        .collection("users")
+        .where("userId", "==", req.user.uid)
+        .limit(1)
+        .get(1);
+    })
+    .then(data => {
+      req.user.handle = data.docs[0].data().handle;
+      req.user.imageUrl = data.docs[0].data().imageUrl;
+      return next();
+    })
+    .catch(err => {
+      console.error("Error while verifying token ", err);
+      return res.status(403).json(err);
+    });
 };
