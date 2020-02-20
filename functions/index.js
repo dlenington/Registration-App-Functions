07ec1c1a4367be426e8f7a9d5acb3445e0332385
Dan Lenington <danlening02@gmail.com>
2020-02-20 15:40:06 -0600

    Debugging
    Debugging getUserData function.

--- functions/index.js
+++ functions/index.js
@@ -145,7 +145,7 @@ app.post("/login", (req, res) => {
 
 app.get("/user", FBAuth, (req, res) => {
   let userData = {};
-  console.log(req.params.handle);
+  console.log(req.params);
   console.log("Logs visible");
   db.doc(`/users/${req.params.handle}`)
     .get()
